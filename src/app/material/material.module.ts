d50c63229723cac9e1f1b6ffb0fa6f86feed68ea
Andy Yu <yyujjiang@hotmail.com>
2018-05-03 22:53:59 +1000

    -- added new button to load more users.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,9 +4,10 @@ import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule],
-    exports: [MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule]
+    imports: [MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule, MatButtonModule],
+    exports: [MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule, MatButtonModule]
 })
 export class MeterialModule {}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort } from '@angular/material';
 import { IUser } from '../user.interface';
 import { UserService } from '../services/user.service';
+const NUM_OF_USERS_TO_LOAD = 5;
 
 @Component({
   selector: 'app-users',
@@ -12,7 +13,8 @@ export class UsersComponent implements OnInit {
   displayedColumns = ['name', 'title', 'DOB', 'picture', 'phone', 'gender', 'country'];
   users: IUser[] = [];
   dataSource = new MatTableDataSource([]);
-
+  numOfUsersToLoad = NUM_OF_USERS_TO_LOAD;
+  
   @ViewChild(MatSort) sort: MatSort;
 
   ngAfterViewInit() {
@@ -35,11 +37,11 @@ export class UsersComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.getUsers();
+    this.getUsers(this.numOfUsersToLoad);
   }
 
-  getUsers(): void {
-    this.userService.getUsers(5)
+  getUsers(numOfUsers: number): void {
+    this.userService.getUsers(numOfUsers)
       .subscribe(loadedUsers => {
         this.users.push(...loadedUsers); 
         this.dataSource.data = this.users;
